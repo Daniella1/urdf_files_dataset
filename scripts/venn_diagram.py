4ae2c6a33e5d319a05d18036e960861ad4d00506
Daniella1 <dani-ella95@live.com>
2023-04-05 13:12:10 +0200

    updated scripts

--- scripts/venn_diagram.py
+++ scripts/venn_diagram.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import subprocess
 
 
+
 # if dataset information not available, then generate
 filename_dataset_information = "dataset_information.csv"
 if not os.path.isfile(filename_dataset_information):
@@ -20,7 +21,30 @@ if not os.path.isfile(filename_dataset_information):
 df = pd.read_csv(filename_dataset_information)
 df = df.fillna("none")
 
-unique_robots = df.name.unique()
-unique_variants = df.variant.unique()
+
+df_original_robots = df[df.variant == "none"] # without variant
+df_variant_robots = df[df.variant != "none"] # non-original
+
+unique_robots = df_original_robots.name.unique() # extract robots without a variant
+unique_variants = df_variant_robots.variant.unique()
+
+df_id_original_robots = pd.DataFrame(columns=["name","id","locations","type"])
+
+
+ids = 0
+for index, row in df_original_robots.iterrows():
+    print(row)
+    # check if robot exists in dataframe, if not then add it
+    if len(df_id_original_robots[df_id_original_robots.name == row.name]) < 1:
+        # add robot to df
+        urdf_path = row.urdf_path.strip('][').replace('"','')
+        urdf_path = urdf_path.strip("WindowsPath('")
+        urdf_path = urdf_path.strip("')")
+        new_row = {'name': row.name, 'id': ids, 'locations': [urdf_path], 'type': row.type}
+        df_id_original_robots = df_id_original_robots.append(new_row, ignore_index=True)
+    else:
+        pass
+    
+    ids += 1
 
 print(df)
