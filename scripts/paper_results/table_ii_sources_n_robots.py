45dcc7dd2fe207e12a6d0490f4bb2e65b0e7c7fa
Daniella1 <dani-ella95@live.com>
2023-06-21 12:14:36 +0200

    updated table_ii script, readme, and meta info

--- scripts/paper_results/table_ii_sources_n_robots.py
+++ scripts/paper_results/table_ii_sources_n_robots.py
@@ -9,7 +9,7 @@ def _extract_meta_information(filename, columns):
         data = json.load(f)
     robots = data['robots']
     for robot in robots:
-        meta_infos = pd.concat([meta_infos, pd.Series({'name':robot['name'],'n_urdfs':len(robot['urdf'])}).to_frame().T], ignore_index=True)
+        meta_infos = pd.concat([meta_infos, pd.Series({'name':robot['name'],'n_robots':len(robot['urdf']), 'variant': "original" if robot['variant'] is None else robot['variant']}).to_frame().T], ignore_index=True)
     return meta_infos
 
 def _extract_source_information(filename):
@@ -17,7 +17,7 @@ def _extract_source_information(filename):
         data = json.load(f)
     return data['source']
 
-dataframe_columns = ['source','n_urdfs','n_robots']
+dataframe_columns = ['source','n_robots','variant']
 sources_n_robots = pd.DataFrame(columns=dataframe_columns)
 
 meta_info_filename = "meta-information.json"
@@ -32,13 +32,19 @@ for d in subdirs:
     assert len(list_of_source_file_paths) == 1, f"Error, expecting one source information file in subdataset, but found either more or none: {list_of_source_file_paths}"
     for file in list_of_meta_file_paths:
         meta_infos = _extract_meta_information(file, dataframe_columns)
-        meta_infos['source'] = _extract_source_information(list_of_source_file_paths[0])
+        meta_infos['source'] = _extract_source_information(list_of_source_file_paths[0])        
         sources_n_robots = pd.concat([sources_n_robots, meta_infos], ignore_index=True)
         
-n_urdfs = list(sources_n_robots[['source','n_urdfs']].groupby(by='source').sum().n_urdfs)
-sources_n_robots = sources_n_robots[['name','source','n_urdfs']].groupby(by='source').count()
-sources_n_robots = sources_n_robots.reset_index().rename({'index':'source','name':'n_robots'}, axis='columns')
-sources_n_robots['n_urdfs'] = n_urdfs
-sources_n_robots = sources_n_robots[['source','n_urdfs','n_robots']]
+
+sorted_sources = ["ros-industrial","matlab","robotics-toolbox","drake","oems","random"]
+variants = sources_n_robots[['source','variant']]
+
+
+sources_n_robots = sources_n_robots[['source','n_robots','variant']].groupby(by='source').count()
+sources_n_robots = sources_n_robots.reset_index().rename({'index':'source','variant': 'n_variants'}, axis='columns')
+for src in sorted_sources:
+    n_variants = len(variants[(variants['source'] == src) & (variants['variant'] != 'original')])
+    sources_n_robots.loc[sources_n_robots['source'] == src,'n_variants'] = n_variants
+
 
 sources_n_robots.to_csv("table_ii_sources_n_robots.csv", index=False)
